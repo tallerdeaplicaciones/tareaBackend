0dc4079640f02132073807f84c47507561f17d4b
Seba-perseverance <scastrof2@outlook.com>
2023-10-24 00:02:22 -0300

    Agregado nuevas funcionalidades al sistema.

--- helpdesk/core/views.py
+++ helpdesk/core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.detail import DetailView
+
+
+class Home():
+    template_name = 'core/inicio.html'
+    context_object_name = 'home'
